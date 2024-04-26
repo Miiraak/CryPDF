b38f90567fa773442573a4690771bfcab97e9854
Miiraak <miiraak@feynlok.ch>
2024-04-27 00:28:15 +0200

    Code modification

--- main.py
+++ main.py
@@ -14,72 +14,95 @@ def crack_pdf(file_path, wordlist_path):
 
     for word in wordlist:
         word = word.strip()
-        if pdf_reader.decrypt(word):
-            print(f"Mot de passe trouvé : {word}")
-            show_password_window(word)
-            return True
+        try:
+            if pdf_reader.decrypt(word):
+                print(f"Password found: {word}")
+                show_password_window(word)
+                return True
+        except NotImplementedError:
+            continue
 
-    print("Mot de passe non trouvé.")
+    print("Password not found.")
     return False
 
 
-def browse_file():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        file_name = os.path.basename(file_path)
-        label.config(text=file_name)
-        global selected_file_path
-        selected_file_path = file_path
+def browse_pdf_file():
+    global selected_pdf_path
+    selected_pdf_path = filedialog.askopenfilename()
+    global pdfName
 
+    if selected_pdf_path:
+        file_nameP = os.path.basename(selected_pdf_path)
+        labelChoiceFile.delete(0, tk.END)
+        labelChoiceFile.insert(0, file_nameP)
+        global selected_wordlist_path
+        selected_wordlist_path = ""
+        pdfName = file_nameP
+
+
+def browse_dictionary_file():
+    global selected_wordlist_path
+    selected_wordlist_path = filedialog.askopenfilename()
+    if selected_wordlist_path:
+        file_nameD = os.path.basename(selected_wordlist_path)
+        labelChoiceDict.delete(0, tk.END)
+        labelChoiceDict.insert(0, file_nameD)
 
 def validate_crack():
-    if selected_file_path:
-        crack_pdf(selected_file_path, wordlist_path)
+    if selected_pdf_path:
+        if selected_wordlist_path:
+            crack_pdf(selected_pdf_path, selected_wordlist_path)
+        else:
+            print("Please select a dictionary file.")
     else:
-        print("Veuillez sélectionner un fichier.")
-
+        print("Please select a PDF file.")
 
 def copy_to_clipboard(password):
     pyperclip.copy(password)
 
-
 def show_password_window(password):
     password_window = Toplevel(root)
-    password_window.title("Mot de passe trouvé")
+    password_window.title("Done !")
     password_window.geometry("250x100")
-    password_label = tk.Label(password_window, text="Mot de passe trouvé :")
+    password_window.resizable(height=False, width=False)
+    password_label = tk.Label(password_window, text=f"Password Found for : {pdfName}")
     password_label.pack(pady=5)
     password_display = tk.Entry(password_window, width=20)
     password_display.insert(0, password)
     password_display.pack(pady=5)
-    copy_button = tk.Button(password_window, text="Copier dans le presse-papiers", command=lambda: copy_to_clipboard(password))
+    copy_button = tk.Button(password_window, text="Copy", command=lambda: copy_to_clipboard(password))
     copy_button.pack(pady=5)
 
 
 root = tk.Tk()
 root.title("CryPDF")
-root.geometry("210x100")
+root.geometry("230x180")
+root.resizable(height=False, width=False)
 
-browseFile_button = tk.Button(root, text="Select", command=browse_file)
+browseFile_button = tk.Button(root, text="PDF", command=browse_pdf_file)
 browseFile_button.pack(pady=10)
-browseFile_button.place(x=15, y=60)
+browseFile_button.place(x=10, y=135)
 
-browse_button = tk.Button(root, text="Select", command=browse_file)
-browse_button.pack(pady=10)
-browse_button.place(x=15, y=60)
+browseDictionary_button = tk.Button(root, text="Dictionary", command=browse_dictionary_file)
+browseDictionary_button.pack(pady=10)
+browseDictionary_button.place(x=75, y=135)
 
-label = tk.Label(root, text="")
-label.pack()
-label.place(x=30, y=30)
+attack_button = tk.Button(root, text=" Attack", command=validate_crack)
+attack_button.pack(pady=10)
+attack_button.place(x=170, y=135)
 
-labelTitre = tk.Label(root, text="File name :")
-labelTitre.pack()
-labelTitre.place(x=10, y=5)
+labelFile = tk.Label(root, text="PDF :")
+labelFile.pack()
+labelFile.place(x=10, y=10)
+labelChoiceFile = tk.Entry(root, width=34)
+labelChoiceFile.pack()
+labelChoiceFile.place(x=10, y=35)
 
-wordlist_path = "C:/Users/Feynlok/Desktop/Developpement/Python/PDF_Cracker/french_passwords_top20000.txt"
-
-validate_button = tk.Button(root, text="Attack", command=validate_crack)
-validate_button.pack(pady=10)
-validate_button.place(x=130, y=60)
+labelDictionary = tk.Label(root, text="Dictionary :")
+labelDictionary.pack()
+labelDictionary.place(x=10, y=70)
+labelChoiceDict = tk.Entry(root, width=34)
+labelChoiceDict.pack()
+labelChoiceDict.place(x=10, y=95)
 
 root.mainloop()
